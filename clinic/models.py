4a1b4a6315f331c2514e69374a78becb59a7f733
nextqq <vskor706@gmail.com>
2019-01-17 15:32:14 +0300

    Boba's commit

--- clinic/models.py
+++ clinic/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class client(models.Model):
+	Fam = models.CharField(max_length=30, verbose_name="Фамилия")
+	Im = models.CharField(max_length=15, verbose_name="Имя")
+	Otch = models.CharField(max_length=20, verbose_name="Отчество")
+	Numm = models.CharField(max_length=15, verbose_name="Телефон")
+
+	def __str__(self):
+		return self.Fam
