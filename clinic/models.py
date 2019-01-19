e1120088e707e20a74b655c3711c7a6903ebcec4
nextqq <vskor706@gmail.com>
2019-01-19 14:14:08 +0300

    Boba

--- clinic/models.py
+++ clinic/models.py
@@ -9,3 +9,16 @@ class client(models.Model):
 
 	def __str__(self):
 		return self.Fam
+
+
+class user(models.Model):
+	Fam = models.CharField(max_length=30, verbose_name="Фамилия")
+	Im = models.CharField(max_length=15, verbose_name="Имя")
+	Otch = models.CharField(max_length=20, verbose_name="Отчество")
+	Numm = models.CharField(max_length=15, verbose_name="Номер телефона")
+	Dolj = models.CharField(max_length=15, verbose_name="Должность")
+	StajR = models.CharField(max_length=15, verbose_name="должность")
+
+	def __str__(self):
+		return self.Fam
+
