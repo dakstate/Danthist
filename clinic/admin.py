2ee46bdb05fd12c93f5e661f21e4f6102fcbc3df
dakstate <shew4encko5548@gmail.com>
2019-01-19 14:50:51 +0300

    sheva trubleshooting

--- clinic/admin.py
+++ clinic/admin.py
@@ -1,22 +1,9 @@
 from django.contrib import admin
-<<<<<<< HEAD
-from .models import client
-<<<<<<< HEAD
-=======
-=======
-from .models import client, content, servers, doctor
->>>>>>> 82b7bd57d678ff0788822ce6441d076910f773a1
+from .models import client, content, servers, doctor, user
 
-
-
+# Register your models here.
 admin.site.register(client)
-<<<<<<< HEAD
->>>>>>> 630afdcf4ba23dbc3489a3f312a40274acf2793e
-=======
 admin.site.register(content)
 admin.site.register(servers)
 admin.site.register(doctor)
->>>>>>> 82b7bd57d678ff0788822ce6441d076910f773a1
-# Register your models here.
-
-admin.site.register(client)
+admin.site.register(user)
