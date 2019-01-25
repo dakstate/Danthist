40861de5db09d812bbbf01d43279e6cb9382465c
nextqq <vskor706@gmail.com>
2019-01-26 00:02:21 +0300

    boba

--- clinic/views.py
+++ clinic/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import HttpResponse, render
-from .models import servers, content
+from .models import servers, content, user
 
 def index(request):
     main = servers.objects.all()
     news = content.objects.all()
-    return render(request,'clinic/index.html',{'context':main, 'news':news})
+    us = user.objects.filter(Type = 'V')
+    return render(request,'clinic/index.html',{'context':main, 'news':news, 'us':us})
 
 def register(request):
     return render(request,'clinic/register.html')
